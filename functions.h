f344b3f3b382d57bccf675c6c90a2ee02381a600
Alexandre Silva <asilva@localhost.localdomain>
2018-05-18 15:00:38 +0100

    menu upgrade

--- functions.h
+++ functions.h
@@ -3,9 +3,9 @@
 
 #include "data.h"
 
-
+void myscannoerrorsforintnum ( int, int, int);
 int ParamReading(int, char *[], char [2][50]);
-void MenuSurfer(int*, int*, int*);
+void MenuSurfer(int*, int*, int*, int*, int*);
 int MainMenu();
 int DataFilter();
 int TemperatureHistory(int*);
